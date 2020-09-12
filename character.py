088fb51da135257cece8713a66bb07fdc532bf63
johnchem <j.payssan@gmail.com>
2020-09-12 20:17:27 +0200

    improvement of the jump mecanic

--- character.py
+++ character.py
@@ -12,65 +12,82 @@ class Character():
 	'''
 	def __init__(self, x, y ,height, width, window):
 		self.x, self.y = x, y
-		self.oldX, self.oldY  = x, y
+		self.dx, self.dy = 0, 0
+		self.jumpHeight = 0
+		self.currentJump = 0
 		self.height, self.width = height, width
 		self.window = window
+		self.rect = pygame.Rect(x, y, width, height)
+		self.gravity = 2
 		self.health = 10
 		self.maxJumps = 10
 		self.jumpsDone = 0
-		self.hitBox = {"top":0, "bottom":0, "left":0,"right":0}
 		self.tiredNess = 0
 		self.onGround = True
 		self.color = (77,77,255)
 
 	def draw(self):
-		self.gravity()
-		pygame.draw.rect(self.window, self.color, 
-						(self.x, self.y, self.width, self.height),0)
+		pygame.draw.rect(self.window, self.color, self.rect,0)
 
 	def touchGround(self):
 		self.onGround = True
 
-	def gravity(self):
-		self.oldPos()
-		self.updateHitbox()
-		if not self.onGround:
-			self.y += 5
+	def move(self, **kwargs):
+		up = kwargs.get('jump', 0)
+		right = kwargs.get('right', 0)
+		left = kwargs.get('left', 0)
 
-	def moveJump(self, value):
-		'''
-			excecute a jump and setting the character off ground
-		'''
-		self.oldPos()
-		if self.onGround:
-			self.jumpsDone += 1
-			self.onGround = False
-			self.y -= value
-			self.updateHitbox()
+		#reset the position delta 
+		self.dx, self.dy = 0, 0
 
-	def moveFwd(self, value):
-		self.oldPos()
-		self.x += value
-		self.updateHitbox()
+		#set the position delta
+		if self.onGround: 
+			if up != 0:
+				self.jumpsDone += 1
+				self.onGround = False
+				self.jumpHeight = up
+				self.currentJump = up
+		else:
+			if self.currentJump < 0: #speed to the top
+				self.dy = -self.currentJump**2 + self.gravity
+				self.currentJump -= self.jumpHeight*0.10
+			else: #speed to the bottom
+				self.dy = self.currentJump**2 + self.gravity
+				self.currentJump -= self.jumpHeight*0.10
+		self.dx = right + left
 
-	def moveBwd(self, value):
-		self.oldPos()
-		self.x -= value
-		self.updateHitbox()
+		#update the rect position
+		self.rect.x += self.dx
+		self.rect.y += self.dy
 
-	def oldPos(self, restore = False):
-		print(" ".join([str(value) for value in self.hitBox.values()]))
-		if restore:
-			self.x, self.y = self.oldX, self.oldY
-			self.updateHitbox()
-		self.oldX, self.oldY = self.x, self.y
 
-	def updateHitbox(self):
-		self.hitBox["top"] = self.y
-		self.hitBox["bottom"] = self.y + self.height
-		self.hitBox["right"] = self.x + self.width
-		self.hitBox["left"] = self.x
+def collisionDetection(char, listObj):
+	for obj in listObj:
+		if char.rect.colliderect(obj.rect):
+			if char.dx > 0: # Moving right; Hit the left side of the obj
+				char.rect.right = obj.rect.left
+			if char.dx < 0: # Moving left; Hit the right side of the obj
+				char.rect.left = obj.rect.right
+			if char.dy > 0: # Moving down; Hit the top side of the obj
+				char.rect.bottom = obj.rect.top
+			if char.dy < 0: # Moving up; Hit the bottom side of the obj
+				char.touchGround()
+				char.rect.top = obj.rect.bottom
 
+def collisionBorder(char, border):
+	rectBorder = border.get_rect()
+	#print(f"{rectBorder.x} {rectBorder.y} {char.rect.x} {char.rect.y}")
+	print(f"{char.rect.left} {char.rect.right} {char.rect.top} {char.rect.bottom}")
+	if not rectBorder.contains(char.rect):
+			if char.dx > 0: # Moving right; Hit the left side
+				char.rect.right = rectBorder.left
+			if char.dx < 0: # Moving left; Hit the right side
+				char.rect.left = rectBorder.right
+			if char.dy > 0: # Moving down; Hit the top side
+				char.touchGround()
+				char.rect.bottom = rectBorder.bottom
+			if char.dy < 0: # Moving up; Hit the bottom side
+				char.rect.top = rectBorder.top
 
 if __name__ == '__main__':
 	pygame.init()
@@ -86,20 +103,24 @@ if __name__ == '__main__':
 		clock.tick(40)
 
 		key = pygame.key.get_pressed()
-		if key[pygame.K_UP]:
-			char.moveJump(50)
-		elif key[pygame.K_RIGHT]:
-			char.moveFwd(10)
-		elif key[pygame.K_LEFT]:
-			char.moveBwd(10)
+		keyPressed = False
 
-		if (char.hitBox["right"] < 10 or char.hitBox["left"] > 1350 or
-			char.hitBox["top"] < 10):
-			char.oldPos(restore = True)
-		elif char.hitBox["bottom"] > 700:
-			char.touchGround()
-			char.oldPos(restore = True)
-			
+		if key[pygame.K_UP]:
+			char.move(jump=-5)
+			keyPressed = True
+
+		if key[pygame.K_RIGHT]:
+			char.move(right=10)
+			keyPressed = True
+
+		if key[pygame.K_LEFT]:
+			char.move(left=-10)
+			keyPressed = True
+
+		if not keyPressed:
+			char.move()
+
+		collisionBorder(char, win)
 
 		#draw element
 		win.fill((255,255,255))
